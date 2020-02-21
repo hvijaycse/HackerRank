887e4a2b58b9e16a2b0a13dbff138314eb260e6a
Harsh Vijay <harshvijay.cse21@jecrc.ac.in>
2020-02-21 17:16:03 +0530

    Spoj Arith got Accepted { learned to read the problem throughly}

--- SPOJ/ARITH.py
+++ SPOJ/ARITH.py
@@ -1,87 +1,57 @@
-import re
-
-def ARITH():
-    expression=input().strip()
-    numbers=re.split('\*|\+|\-',expression)
-    operator=expression[len(numbers[0])]
-    outputs=[]
-    biggest=0
-    length=len(numbers[0])
-    outputs.append( [ length,
-                        numbers[0] ] 
-                    )
-    if biggest<length:
-        biggest=length
+def Calculate(Expression, Operator):
+    Return_String = ""
+    output = []
+    Index = Expression.index(Operator)
+    First = Expression[ : Index ]
+    Second = Expression[ Index: ]
+    Answer = str(eval(Expression))
+    maximum = 0
+    for values in [First, Second]:
+        Length = len(values)
+        output.append((Length, values))
+        if Length > maximum:
+            maximum = Length
+    
+    if Operator == '*' and len(Second[1:]) != 1:
         
-    length=len(numbers[1])+1
-    outputs.append( [ length,
-                    operator + numbers[1] ]
-                    )
-    if biggest<length:
-        biggest=length
+        dashes = max( len(Second), len( str( eval( 
+                                                  Second[-1] +
+                                                   '*' +
+                                                   First )) ))
+        output.append( ( dashes, '-' * dashes ))
+        Second = Second[1:]
+        Second_Len = len(Second)
+        times = Second_Len
+        while times:
+            Muli = str( eval( Second[ times -1] + 
+                             '*' + 
+                             First ))
+            Muli = Muli + ' ' * ( Second_Len - times )
+            current_len = len( Muli)
+            output.append( ( current_len, Muli))
+            if current_len > maximum:
+                maximum =  current_len
+            times = times - 1 
+        Second = Muli
+    dashes = max( len( Second), len(Answer))
+    output.append( (dashes, '-' * dashes ))
+    output.append( ( len( Answer), Answer))            
+    for values in output:
+        Return_String += ' ' * (maximum - values[0] ) + values[1] +'\n'
+    return Return_String + '\n'
     
-    outputs.append( [ biggest ,
-                        '-'*biggest ])
-    
-    # now cheking for operator
-    
-    if '+' in expression:
-        answer=str( int( numbers[0] ) + int( numbers[1] ) )
-        length=len(answer)
-        
-        outputs.append( [ length,
-                         answer ] )
-        if length>biggest:
-            biggest=length
             
-    if '-' in expression:
-        answer=str( int( numbers[0] ) -  int( numbers[1] ) )
-        length=len(answer)
-        
-        outputs.append( [ length,
-                         answer ] )
-        if length>biggest:
-            biggest=length      
-        
-    
-    if '*' in expression:
-        first=int(numbers[0])
-        answer=str(first*int(numbers[1]))
-        if len(answer)>biggest:
-                biggest=len(answer)
-        length-=1
-        i=0
-        while length:
-            length-=1
-            product=int( numbers[1][length] ) * first
-            product=str( product )
-            temp_len=len( product)+i
-            outputs.append( [ temp_len,
-                             product ] )
-            if temp_len>biggest:
-                biggest=temp_len
-            i+=1
-        if len( answer[1] ) -1:
-            outputs.append( [ biggest,
-                            '-'*biggest ] )
-            
-            outputs.append( [ len(answer),
-                            answer ] )
-        
-    for answer in outputs:
-        print( ' '*( biggest - answer[0] ) + answer[1] )
-    print('')
-    return
-            
-        
-                
-            
-        
-    
-    
 if __name__=='__main__':
     times=int(input().strip())
+    output = ""
     while times:
-        ARITH()
+        Expression = input()
+        if '+' in Expression:
+            output += Calculate(Expression, '+')
+        elif '-' in Expression:
+            output += Calculate(Expression, '-')
+        else:
+            output += Calculate(Expression, '*')
         times-=1
+    print(output)
     exit(0)
