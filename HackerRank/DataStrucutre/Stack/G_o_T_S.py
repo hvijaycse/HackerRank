709bbc894981fc7aedac5b9876cb1fff20a4cdc0
Harsh Vijay <harshvijay.cse21@jecrc.ac.in>
2020-02-24 18:52:04 +0530

    Solved Game of Two stack learned new concept

--- HackerRank/DataStrucutre/Stack/G_o_T_S.py
+++ HackerRank/DataStrucutre/Stack/G_o_T_S.py
@@ -1,62 +1,30 @@
-#!/bin/python3
 
-import os
-import sys
-
-#
-# Complete the twoStacks function below.
-#
-class Stacking():
-    
-    def __init__(self, Array):
-        self.Stack = Array
-        self.Head = 0
-        self.Length = len(Array)
-    
-    def pop(self ):
-        self.Head = self.Head + 1
-        return self.Stack[self.Head - 1]   
-    
-    def Last(self ):
-        return self.Stack[self.Head]
-    def Check(self ):
-        return self.Length - self.Head
-    
-def twoStacks(x, a, b):
-    Stack1 = Stacking(a)
-    Stack2 = Stacking(b)
-    Summision = 0
-    steps = 0    
-    while Summision < x -1 :
-        steps += 1
-        if Stack1.Stack:
-            Minimum = Stack1
-        else:
-            Minimum = Stack2
-        if  Stack2.Stack :
-            if Stack2.Last() < Minimum.Last():
-                Minimum = Stack2
-        Summision += Minimum.pop()
-    return steps
 
 if __name__ == '__main__':
-
-    g = int(input())
-
-    for g_itr in range(g):
-        nmx = input().split()
-
-        n = int(nmx[0])
-
-        m = int(nmx[1])
-
-        x = int(nmx[2])
-
-        a = list(map(int, input().rstrip().split()))
-
-        b = list(map(int, input().rstrip().split()))
-
-        result = twoStacks(x, a, b)
-        print(result)
-
-
+    games = int( input())
+    
+    while games:
+        games = games - 1
+        i = 0
+        j = 0
+        steps = 0
+        SUM = 0
+        N, M, Max = map( int, input().split())
+        A = list( map(int, input().split() ))    
+        B = list( map(int, input().split() ))
+        while i < N and SUM + A[i] <= Max :
+            SUM = SUM + A[i]
+            i = i + 1
+        steps = i
+        while j < M and i > -1 :
+            SUM =  SUM + B[j]
+            j = j + 1
+            while SUM > Max and i > 0:
+                i = i - 1
+                SUM = SUM - A[i]
+            if SUM <= Max and (i + j) > steps :
+                steps = i + j
+        
+        
+        print(steps) 
+    exit(0)
