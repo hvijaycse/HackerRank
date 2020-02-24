e82bed5e5ed4ab8eb2ff4848091d1f95903e7518
Harsh Vijay <harshvijay.cse21@jecrc.ac.in>
2020-02-25 01:04:16 +0530

    fuck after managing so much concentration, i was able to build faster and accurate algo still its slow, fuck

--- HackerRank/DataStrucutre/Stack/PoisnousPlant.py
+++ HackerRank/DataStrucutre/Stack/PoisnousPlant.py
@@ -1,3 +1,34 @@
+def Dating( Before, Stack):
+    print( Before, Stack)
+    Max_Days = 0
+    while Stack:
+        TOP = Stack.pop()
+        if TOP <= Before:
+            Before = TOP
+        else:
+            TO_Be_Breaked = False
+            Days = 1
+            MINI = []
+            Biggest = TOP
+            while Stack:
+                New = Stack.pop()
+                if New <= Before:
+                    Before = New
+                    break
+                if New <= Biggest:
+                    MINI.append( New)
+                    Days = Days + 1
+                else:
+                    TO_Be_Breaked = True
+                Biggest = New
+            if TO_Be_Breaked:
+                MINI.reverse()
+                Days = 1 + Dating( Before, MINI)
+            if Days > Max_Days:
+                Max_Days = Days
+    return Max_Days
+
+
 if __name__ == "__main__":
     # file = open('32400.txt')
     # Number = int(file.readline())
@@ -6,29 +37,7 @@ if __name__ == "__main__":
     Plants =  list( map(int, input().split() ) )
     # Plants =  list( map(int, file.readline().split() ) )
     Max_Days = 0
-    Must = Plants[0]
-    Index = 1
-    Last = Number
-    Prev_MIn = float('-inf')
-    flag = False
-    while Index < Last:
-        if Plants[Index] <= Must:
-            Must = Plants[Index]
-            Index += 1
-        else:
-            Days = 1
-            while  Index + 1 < Last and Plants[Index + 1] > Plants[Index]:
-                Index = Index + 1
-            while Index + 1 < Last and Plants[Index + 1] >= Must and Plants[Index + 1] < Plants[Index]:
-                flag = True
-                Last_Min = Plants[ Index + 1]
-                Index = Index + 1
-                Days = Days + 1
-            Index += 1
-            if flag:
-                if Last_Min < Prev_MIn:
-                    Days = Days + 1
-                Prev_MIn = Last_Min
-            if Days > Max_Days:
-                Max_Days = Days
-    print(Max_Days)
+    Plants.reverse()
+    Before = Plants.pop()
+    Days = Dating(Before, Plants)
+    print(Days)
