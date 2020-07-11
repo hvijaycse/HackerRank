1d544f7fccf80ab365a285fc1ba415f60b43c19b
Harsh Vijay <hvijay.cse@gmail.com>
2020-07-11 14:52:53 +0530

    finally, solved the bug

--- Code_chef_july_long/DoctorChef.py
+++ Code_chef_july_long/DoctorChef.py
@@ -4,58 +4,43 @@ def main():
     while TestCase:
         TestCase -= 1
         N, x = map( int, input().split() )
-        xtmp = x
         Countries_Patient = list( map( int, input().split() ) )[ : N]
         Countries_Patient.sort()
-        Tmp_Patient = [a for a in Countries_Patient]
-        Days1 = 0 
-        Days2 = 0 
+        Days = 0 
         Start = 0 
         Max = len( Countries_Patient)
-        while  Start < Max and Countries_Patient[Start] < x:
+        while  Start + 1 < Max and Countries_Patient[Start] < x:
             Start += 1
-        tmpStart = Start
         if Start > 0:
-            tmpStart = Start - 1
+            count1 = 0
+            count2 = 1
+            tmpx = x
+            while tmpx < Countries_Patient[Start]:
+                tmpx = tmpx << 1
+                count1 += 1
+            tmpx = Countries_Patient[ Start -1 ] * 2
+            while tmpx < Countries_Patient[ Start]:
+                tmpx = tmpx << 1
+                count2 += 1
+            if count2 <= count1:
+                Start = Start - 1
         forward = Start
         Backward = Start - 1
         while forward < Max:
-            # print( Countries_Patient, x, forward)
-            Days1 += 1
+            Days += 1
             if x >= Countries_Patient[forward]:
                 x = Countries_Patient[forward]
                 Countries_Patient[forward] = 0
                 forward += 1
             x = x << 1
         while Backward >= 0:
-            Days1 += 1
-            # print( Countries_Patient, x)
+            Days += 1
             if x >= Countries_Patient[ Backward]:
                 x = Countries_Patient[Backward]
                 Countries_Patient[ Backward] = 0
                 Backward -= 1
             x = x << 1
-        
-        forward = tmpStart
-        Backward = tmpStart - 1
-        while forward < Max:
-            # print( Countries_Patient, x, forward)
-            Days2 += 1
-            if xtmp >= Tmp_Patient[forward]:
-                xtmp = Tmp_Patient[forward]
-                Tmp_Patient[forward] = 0
-                forward += 1
-            xtmp = xtmp << 1
-        while Backward >= 0:
-            Days2 += 1
-            # print( Tmp_Patient, xtmp)
-            if xtmp >= Tmp_Patient[ Backward]:
-                xtmp = Tmp_Patient[Backward]
-                Tmp_Patient[ Backward] = 0
-                Backward -= 1
-            xtmp = xtmp << 1
-        print( min(( Days1, Days2)))
-            
+        print( Days)   
 if __name__ == "__main__":
     main()
     exit(0)
