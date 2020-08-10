789bec49d5697820baf3d804d34850cf49237f94
Harsh Vijay <hvijay.cse@gmail.com>
2020-08-11 00:35:40 +0530

    finally solved the LPS

--- Leetcode/LPS.py
+++ Leetcode/LPS.py
@@ -1,4 +1,40 @@
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
+    def getPlain(self, Index, array):
+        ans = array[Index][0]*array[Index][1]
+        back = Index - 1
+        front = Index + 1   
+        while back >= 0 and front < len( array):
+            if array[back][0] == array[front][0]:
+                if array[back][1] == array[front][1]:
+                    ans = array[back][0]* array[ back][1] + ans + array[back][0]* array[ back][1] 
+                    back -= 1
+                    front += 1
+                else:
+                    minimum = min( array[back][1], array[front][1])
+                    ans = array[back][0]* minimum + ans + array[back][0]* minimum
+                    break
+            else:
+                break    
+        return ans   
 
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) >1:
+            array = []
+            start = 0
+            while start < len( s):
+                end = start + 1
+                while end < len(s) and s[end] == s[start]:
+                    end += 1
+                array.append( ( s[start], end - start) )
+                start = end
+            ans = ''
+            for _ in range( array[0][1]):
+                ans += array[0][0]
+            for Index in range( 1, len( array)):
+                tmp_ans = self.getPlain(Index, array)
+                if len( tmp_ans) > len( ans):
+                    ans = tmp_ans
+            return ans 
+        else:
+            return s
         
