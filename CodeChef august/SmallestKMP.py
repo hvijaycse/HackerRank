5449b5bfce3d14960e349ff959d4db3179ee72ff
Harsh Vijay <hvijay.cse@gmail.com>
2020-08-11 13:58:53 +0530

    Yaay solved Smallest KMP small issue was there

--- CodeChef august/SmallestKMP.py
+++ CodeChef august/SmallestKMP.py
@@ -1,24 +1,28 @@
 def Int():
-    return int( input() )
+    return int(input())
+
 
 def List():
     return input().split()
 
+
 def Ilist():
-    return list( map( int, input().split()))
+    return list(map(int, input().split()))
+
 
 def yes():
     print('YES')
 
+
 def no():
     print('NO')
+
 
 def main():
     TestCase = Int()
     for _ in range(TestCase):
         S = input()
         Pattern = input()
-        Smaller = False
         CountDict = {}
         for char in S:
             if char in CountDict:
@@ -27,17 +31,21 @@ def main():
                 CountDict[char] = 1
         for char in Pattern:
             CountDict[char] -= 1
-            if ord( char) < ord( Pattern[0]):
-                Smaller = True
+        front = True
+        i = 0
+        while i < len(Pattern) and Pattern[0] == Pattern[i]:
+            i += 1
+        if i < len(Pattern) and ord(Pattern[i]) > ord(Pattern[0]):
+            front = False
 
         Forward = []
         Backword = []
         for char, count in CountDict.items():
             if count > 0:
-                if ord( char) < ord(Pattern[0]):
-                    Forward += [ char for _ in range(count)]
-                elif ord( char) == ord( Pattern[0]):
-                    if  not Smaller:
+                if ord(char) < ord(Pattern[0]):
+                    Forward += [char for _ in range(count)]
+                elif ord(char) == ord(Pattern[0]):
+                    if not front:
                         Forward += [char for _ in range(count)]
                     else:
                         Backword += [char for _ in range(count)]
@@ -45,9 +53,8 @@ def main():
                     Backword += [char for _ in range(count)]
         Forward.sort()
         Backword.sort()
-        print( ''.join( Forward) + Pattern + ''.join( Backword))
-                
-        
+        print(''.join(Forward) + Pattern + ''.join(Backword))
+
 
 if __name__ == "__main__":
     main()
